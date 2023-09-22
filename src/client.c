506009055d5dbdccf51a0c9c16e4b58612e43162
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2023-09-22 18:57:05 -0400

    Add functionality for server to send back to client

--- src/client.c
+++ src/client.c
@@ -21,45 +21,53 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in sin;
     char *host;
     char buf[MAX_LINE];
-    int fd;
-    if (argc==2) {
+    int server_fd;
+    if (argc == 2) {
         host = argv[1];
-    }
-    else {
+    } else {
         fprintf(stderr, "usage: %s <host>\n", argv[0]);
         exit(1);
     }
 
-    /* translate host name into peer.fd IP address */
+    /* translate host name into peer.server_fd IP address */
     host_pointer = gethostbyname(host);
     if (!host_pointer) {
-        fprintf(stderr, "%s: unknown host: %fd\n", argv[0], host);
+        fprintf(stderr, "%s: unknown host: %server_fd\n", argv[0], host);
         exit(1);
     }
     /* build address data structure */
-    bzero((char *)&sin, sizeof(sin));
+    bzero((char *) &sin, sizeof(sin));
     sin.sin_family = AF_INET;
-    bcopy(host_pointer->h_addr, (char *)&sin.sin_addr, host_pointer->h_length);
+    bcopy(host_pointer->h_addr, (char *) &sin.sin_addr, host_pointer->h_length);
     sin.sin_port = htons(PORT);
     /* active open */
-    if ((fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((server_fd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
         perror("client: socket");
         exit(1);
     }
-    if (connect(fd, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+    if (connect(server_fd, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
         perror("client: connect");
-        close(fd);
+        close(server_fd);
         exit(1);
     }
 
     printf("Connection established!\n");
 
     /* main loop: get and send lines of text */
-    do  {
-        printf("Enter a message: ");
-        buf[MAX_LINE-1] = '\0';
-        send(fd, buf, strlen(buf) + 1, 0);
-    } while (fgets(buf, sizeof(buf), stdin));
+    char *ret;
+    do {
+        printf("\x1b[33mEnter a message: \x1b[0m");
+        ret = fgets(buf, sizeof(buf), stdin);
+
+        buf[MAX_LINE - 1] = '\0';
+        send(server_fd, buf, strlen(buf) + 1, 0);
+
+        // receive message from server
+        recv(server_fd, buf, sizeof(buf), 0);
+
+        printf("\x1b[32mServer responded: \x1b[0m%s\n", buf);
+
+    } while (ret);
 
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -4,27 +4,31 @@
 
 #include "../include/server.h"
 #include <stdio.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <stdlib.h>
-#include <strings.h>
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
 
 #define SERVER_PORT 5432
 #define MAX_PENDING 5
 #define MAX_LINE 256
 
-int main()
-{
+void print_all_chars(char *buf, unsigned long size);
+
+char *get_date_and_time();
+
+
+int main() {
     struct sockaddr_in sin;
     char buf[MAX_LINE];
+    char ret[MAX_LINE];
     int len;
     int fd, client_fd;
     /* build address data structure */
-    bzero((char *)&sin, sizeof(sin));
+    bzero((char *) &sin, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(SERVER_PORT);
@@ -36,7 +40,7 @@ int main()
         perror("server: socket");
         exit(1);
     }
-    if ((bind(fd, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+    if ((bind(fd, (struct sockaddr *) &sin, sizeof(sin))) < 0) {
         perror("server: bind");
         exit(1);
     }
@@ -45,26 +49,44 @@ int main()
     printf("Server is listening on port %d\n", SERVER_PORT);
 
     /* wait for connection, then receive and print text */
-    while(1) {
-        if ((client_fd = accept(fd, (struct sockaddr *)&sin, &len)) < 0) {
+    while (1) {
+        if ((client_fd = accept(fd, (struct sockaddr *) &sin, &len)) < 0) {
             perror("server: accept");
             exit(1);
         }
-        while (len = recv(client_fd, buf, sizeof(buf), 0)){
+        printf("Connected to %s successfully!\n", inet_ntoa(sin.sin_addr));
+        do {
+            len = recv(client_fd, buf, sizeof(buf), 0);
 
-            /* Logic to strip the NULL chars */
-            int i;
-            for(i=0;i<sizeof(buf);i++) {
-                if(buf[i] != '\0')
-                    printf("%c", buf[i]);
-            }
+            printf("Received message: ");
+            print_all_chars(buf, sizeof(buf));
 
-            /* No need to fputs, as we are already printing
-               one char at a time */
-            //fputs(buf, stdout);
+            char *date_and_time = get_date_and_time();
+
+            sprintf(ret, "On %s you said: %s", date_and_time, buf);
+
+            // send message to client
+            send(client_fd, ret, sizeof(ret), 0);
+
+            bzero(ret, sizeof(ret));
             bzero(buf, sizeof(buf));
             fflush(stdout);
-        }
+        } while (len);
         close(client_fd);
     }
+}
+
+void print_all_chars(char *buf, unsigned long size) {
+    for (int i = 0; i < size; i++) {
+        if (buf[i] != '\0')
+            printf("%c", buf[i]);
+    }
+}
+
+char *get_date_and_time() {
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    char *s = asctime(tm);
+    s[strlen(s) - 1] = '\0';
+    return s;
 }
